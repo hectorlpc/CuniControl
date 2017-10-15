991a7f491e5ac16edb364380079a8f0152fe3a1f
Roger Romero <r.romero.o@outlook.com>
2017-10-14 20:49:00 -0500

    Login con diseño instalado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,25 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Inicio de sesion</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('CUP') ? ' has-error' : '' }}">
-                            <label for="CURP" class="col-md-4 control-label">CURP</label>
-
-                            <div class="col-md-6">
-                                <input id="CURP" type="name" class="form-control" name="CURP" value="{{ old('CURP') }}" required autofocus>
-
-                                @if ($errors->has('CURP'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('CURP') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Contraseña</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Recuérdame
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Iniciar sesión   
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    ¿Olvidaste tu contraseña?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>CuniControl | Inicio</title>
+        <link rel="stylesheet" type="text/css" href="{{ URL::to('/') }}/css/login.css">
+    </head>
+    <body>
+        <div class="loginBox">
+            <img src="{{asset('images/LOGO CUNICONTROL.png')}}" class="user">
+            <h2>BIENVENIDO A CUNICONTROL</h2>
+            <form method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
+                <p>Curp</p>
+                <input type="text" name="CURP" placeholder="Ingresa CURP">
+                <p>Contraseña</p>
+                <input type="Password" name="password" placeholder="••••••">
+                <input type="submit" name="" value="Iniciar">
+                <a href="{{url('/register')}}">¿Aún no tienes cuenta? Registrate aquí</a>
+                <center><a href="#">¿Olvidaste tu contraseña?</a>
+                
+            </form>
         </div>
-    </div>
-</div>
-@endsection
+    </body>
+</html>

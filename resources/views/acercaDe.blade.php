10c23f63dd3de4147c0b8d3fc2dd0e82612e1fd9
Erick MN <erickmarteln@gmail.com>
2017-11-17 01:59:42 -0600

    vista acerca de actualizada

--- resources/views/acercaDe.blade.php
+++ resources/views/acercaDe.blade.php
@@ -1,38 +1,133 @@
-@extends('layouts.Bienvenida')
+﻿@extends('layouts.Principal')
 @extends('layouts.menu')
 @section('content')
-
       <div class="container">
        
     <figure>
-        <left><img src="images/empresa.png" alt=""></left>
-        <img src="images/fesc.png" alt="" width="217" height="190" align="right">
+        <left><img src="images/empresa.png" alt="" width="130" height="130" align="left"></left>
+        <img src="images/fesc.png" alt="" width="140" height="140" align="right">
     </figure>
-      <br>
-      <br>
-       <center><h2>Equipo de desarrollo:</h2></center>
+       <center><h1>Equipo de desarrollo:</h1></center>
       </br>
+      <center><h2>Licenciatura en informática</h2></center>
     </br>
-      
-      <center><p> Romero Ortiz Rogelio
+      <center><table>
+        <tr>
+          <th>Por orden alfabético:</th><td>
+          <th>Cargos:</th>
           <br>
-          Martel Navarrete Erick Rood
+        </tr>
+        <tr>
+          <td>1.- Cabrera Cordero Piero Abraham.</td>
+          <td><td><br>-Soporte General 
+            <br>
+          -Tester
           <br>
-          Calleja Quintanar Andrea Lizeth
+          -Responsable de documentación
           <br>
-          Dominguez Cholula Aldo Gustavo
+          -Documentación 
           <br>
-          Lopez Palma Hector
+        </td>
+        </tr>
+        <tr>
+          <td>2.- Calleja Quintanar Andrea Lizeth.</td>
+          <td><td> <br><br><br>-Responsable del diseño de interfaces
+            <br>
+          -Diseñadora de interfaces
           <br>
-          Vega Perea Jake
+          -Analista
           <br>
-          Cabrera Cordero Piero Abraham
-          <br>
-          Mendoza Servin Alberto
-          <br>
-          Hernandez Becerril Rene
-          </p></center>
-          <br>
-          <br>
-      </div>
+        </td>
+        </tr>
+        <tr>
+          <td>3.- Domínguez Cholula Aldo Gustavo.</td>
+          <td><td> <br><br><br>-Diseñador de interfaces
+            <br>
+            -Analista
+            <br>
+            -Diseño del logotipo de la empresa
+            <br>
+          </td>
+        </tr>
+        <tr>
+          <td>4.- Hernández Becerríl Rene</td>
+          <td><td> <br><br><br>-Tester
+            <br>
+            -Documentador
+            <br>
+            -Analista
+            <br>
+            </td>
+        </tr>
+        <tr>
+          <td>5.- López Palma Hector.</td>
+          <td><td> <br><br><br>-Responsable de la base de datos
+            <br>
+            -Desarrollo de la base de datos
+            <br>
+            -Diseñador de interfaces
+            <br>
+            -Analista
+            </td>
+        </tr>
+        <tr>
+          <td>6.- Martel Navarrete Erick Rood.</td>
+          <td><td> <br><br><br>-Lider del proyecto
+            <br>
+            -Desarrollo de la base de datos
+            <br>
+            -Programador
+            <br>
+            -Analista
+            <br>
+            </td>
+        </tr>
+        <tr>
+          <td>7.- Mendoza Servín Alberto.</td>
+          <td><td> <br><br><br>-Soporte general
+            <br>
+            -Tester
+            <br>
+            -Documentador
+            <br>
+            </td>
+        </tr>
+        <tr>
+          <td>8.- Romero Ortíz Rogelio.</td>
+          <td><td> <br><br><br>-Responsable de programación
+            <br>
+            -Programador
+            <br>
+            -Desarrollo de la base de datos
+            <br>
+            -Analista
+          </td>
+        </tr>
+        <tr>
+          <td>9.- Vega Pera Jake.</td>
+          <td><td> <br><br><br>-Diseñador de interfaces
+            <br>
+            -Responsable de minutas
+            <br>
+            -Tester
+            <br>
+            -Documentador
+            <br>
+            </td>
+        </tr>
+        <tr>
+          <td>10.-García Cortés David Antonio</td>
+          <td><td> <br><br><br>-Diseño de logotipo del sistema
+            <br>
+            </td>
+        </tr>
+      </table></center><br><br>
+
+	   <center><h2>Contacto: cunicontrol@gmail.com</h2></center>
+	   <center><h5><i>© Derechos Reservados 2017 BitBox Systems S.A de C.V. Todos los Derechos Reservados.</i></h5></center>
+     	   <center><h7>Todos los contenidos de este Sistema(Incluyendo, pero no limitado a, texto, logotipos, contenido, fotografías, botones y nombres comerciales) están sujetos a derechos de propiedad por las leyes de Derechos de Autor a <i><b>BitBox Systems S.A de C.V.</i></b>de terceros titulares de los mismos que han autorizado debidamente su inclusión.</h7></center> 
+     	   <center><h6>Queda prohibido copiar, reproducir, distribuir, publicar, transmitir, difundir, o en cualquier modo explotar cualquier parte de este servicio sin la autorización previa por escrito de <i><b>BitBox Systems S.A de C.V.</i></b> o de los titulares correspondientes. </h6></center>	
+	 </div>
+
+    </section>
 @endsection

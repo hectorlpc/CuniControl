0a6f2d4ad88b7a13e5412b850ce5c2cf2a83f74c
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2017-11-16 22:07:02 -0600

    Agregar scope al modelo Parto para consultar numero de gazapos vivos o muertos agrupados por raza

--- app/Parto.php
+++ app/Parto.php
@@ -18,5 +18,15 @@ class Parto extends Model
     	return $this->belongsTo(Monta::class,'Id_Monta','Id_Monta');
     //	return $this->belongsTo('App\Monta','Id_Monta','Id_Monta');
     }
+
+    public function scopeNumeroGazapos($query, $status = 'Vivos') {
+        $columna = $status == 'Vivos' ? 'Numero_Vivos' : 'Numero_Muertos';
+
+        return $query->select(\DB::raw('Raza.Nombre_Raza, SUM(Parto.' . $columna . ') AS Numero'))
+            ->join('Monta', 'Parto.Id_Monta', '=', 'Monta.Id_Monta')
+            ->join('Conejo', 'Monta.Id_Conejo_Hembra', '=', 'Conejo.Id_Conejo')
+            ->join('Raza', 'Conejo.Id_Raza', '=', 'Raza.Id_Raza')
+            ->groupBy('Raza.Nombre_Raza');
+    }
 }
 

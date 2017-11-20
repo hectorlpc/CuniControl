045de6f29f3a17db87c04f13c8c9cf4b5dad5ce7
Hectorc <hectorcoreylp@gmail.com>
2017-11-20 01:23:29 -0600

    Correo con usuario y contraseña

--- resources/views/Correo/Plantillacorreo.blade.php
+++ resources/views/Correo/Plantillacorreo.blade.php
@@ -10,6 +10,8 @@
     <div class="alert alert-success" role="alert">
     <h4 class="alert-heading">Hola {{$Nombre_Usuario}}</h4>
     <p>Bienvenido a CuniControl</p>
+    <h3>Tu Usuario es: {{$CURP}}</h3>
+    <h3>Tu Contraseña es: {{$password}}</h3>
     <p>Por Favor Confirma tu Correo</p>
     <hr>
     <a class="" href="{{url('/Usuario/activacion/'.$confirmacioncode)}}">Confirmar</a>
